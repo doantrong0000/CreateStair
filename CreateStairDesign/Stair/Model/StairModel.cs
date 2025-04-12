949b5c9994808eeab62b958095167558d5ac715b
doantrong0000 <doantrong0000@gmail.com>
2025-04-12 17:33:38 +0700

    dat thang

---
+++ CreateStairDesign/Stair/Model/StairModel.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateStairDesign.Stair.Model
+{
+    public class StairModel
+    {
+        public double Daythang1 { get; set; }
+        public double Daythang2 { get; set; }
+        public double Daythang3 { get; set; }
+        public double Daythang4 { get; set; }
+        public double b1 { get; set; }
+        public double b2 { get; set; }
+        public double b3 { get; set; }
+        public double h1 { get; set; }
+        public double h3 { get; set; }
+        public double RongThang12 { get; set; }
+        public double RongThang3 { get; set; }
+        public double off { get; set; }
+        public double y2 { get; set; }
+    }
+}
--- CreateStairDesign/Stair/ViewModel/CreateStairViewModel.cs
+++ CreateStairDesign/Stair/ViewModel/CreateStairViewModel.cs
@@ -9,13 +9,17 @@ using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using CommunityToolkit.Mvvm.Input;
 using CreateStairDesign.Stair.View;
+using BimSpeedUtils;
 using System.Windows.Shapes;
+using Serilog.Core;
+using CommunityToolkit.Mvvm.DependencyInjection;
 
 namespace CreateStairDesign.Stair.ViewModel
 {
     public class CreateStairViewModel : ObservableObject
     {
         public UIDocument UiDocument { get; set; }
+        public Document doc { get; set; }
         public CreateStairView MainView { get; set; }
 
         private string path { get; set; }
@@ -29,15 +33,84 @@ namespace CreateStairDesign.Stair.ViewModel
             }
         }
 
+        public FamilySymbol FamilyStair { get; set; }
+
         public RelayCommand OpenFileCmd { get; set; }
+        public RelayCommand RunCmd { get; set; }
 
         public CreateStairViewModel(UIDocument uiDocument)
         {
             UiDocument = uiDocument;
+            doc = uiDocument.Document;
             OpenFileCmd = new RelayCommand(OpenFileJson);
-
+            RunCmd = new RelayCommand(Run);
+            LoadFamily();
+            
         }
+        private void LoadFamily()
+        {
+           try
+            {
+                var thang = new FilteredElementCollector(doc)
+                   .OfClass(typeof(FamilySymbol))
+                   .OfCategory(BuiltInCategory.OST_StructuralFraming).Cast<FamilySymbol>()
+                   .FirstOrDefault(x => x.Name =="BS_Stair_test");
+                FamilyStair = thang;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }   
+        }
+        private void AssignValue()
+        {
+            try
+            {
+             
+            }
+            catch (Exception ex)
+            {
+               System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+        private void Run()
+        {
+       
+            if (FamilyStair == null)
+            {
+                System.Windows.MessageBox.Show("Không tìm thấy family 'BS_Stair_test'");
+                return;
+            }
 
+            if (!FamilyStair.IsActive)
+            {
+                FamilyStair.Activate();
+                doc.Regenerate();
+            }
+            AssignValue();
+            MainView.Close();
+            XYZ point = UiDocument.Selection.PickPoint("Chọn điểm để đặt cầu thang");
+            
+            
+            using (Transaction trans = new Transaction(doc, "Place Stair Family"))
+            {
+                trans.Start();
+
+                // Dùng dạng NonStructural nếu đặt bằng điểm
+                var stair = doc.Create.NewFamilyInstance(
+                    point,
+                    FamilyStair,
+                    StructuralType.NonStructural // CHÚ Ý: beam thường yêu cầu Line
+                );
+                Parameter paramB1 = stair.LookupParameter("b1");
+                if (paramB1 != null && !paramB1.IsReadOnly)
+                {
+                    paramB1.Set(5000.MmToFoot()); // đặt 500mm (chuyển về foot)
+                }
+
+                trans.Commit();
+            }
+        }
         private void OpenFileJson()
         {
             System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
